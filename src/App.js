81b0b582f8731e19a4845d0fcbe8dcc2afcf1bf4
peter <peternyangon@gmail.com>
2023-01-02 16:56:53 -0600

    added handleNameChange funtion to app file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleNameChange = () => {
+    const name = ['Peter','Simon','Millicent','Sifa','Joshua']
+    const int = Math.floor(Math.random()*5);
+    return name[int];
+  }
+  const name="Simon"
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to see what i have change.
         </p>
         <a
           className="App-link"
@@ -17,6 +24,8 @@ function App() {
         >
           Learn React
         </a>
+        <h1>Random name generator: {handleNameChange()}</h1>
+        <p>my name is:{name}</p>
       </header>
     </div>
   );
